518c529f56d8c7dad071cbbf13b3a185785af8da
Stian.Munkejord <smu027@uib.no>
2020-10-02 14:20:36 +0200

    Tester 3 = Pygame

--- Testers/Tester3.py
+++ Testers/Tester3.py
@@ -1,6 +1,22 @@
-N = "Hello"*3
-S = "9"*3
-if N.isdigit():
-    print("Yes")
+import pygame
+pygame.init()
 
-print("Bye")
+win = pygame.display.set_mode((500, 500))
+
+pygame.display.set_caption("First game")
+
+x = 50
+y = 50
+width = 40
+height = 60
+vel = 5
+
+run = True
+while run:
+    pygame.time.delay(100)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+pygame.quit()
